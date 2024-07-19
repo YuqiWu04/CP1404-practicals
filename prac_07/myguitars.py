b0ecc21ee08fa14db8dc5c63af4c56222b06fe2b
yukey <yuqi.wu1@my.jcu.au>
2024-07-19 14:15:46 +0800

    prac7

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,4 +1,4 @@
-import csv
+
 from prac_07.guitar import Guitar
 
 FILENAME = "guitars.csv"
@@ -14,6 +14,9 @@ def main():
     guitar = [guitar for guitar in guitars]
     for a in guitar:
         print(a)
+    get_valid_guitar_data(guitars)
+    upload_file(guitars)
+
 
 def read_file():
     guitars = []
@@ -26,6 +29,22 @@ def read_file():
             guitars.append(Guitar(name, year, cost))
     return guitars
 
+
+# Complete first part of More Guitars exercise
+def get_valid_guitar_data(guitars):
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost:$ "))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        name = input("Name: ")
+    return guitars
+
+def upload_file(guitars):
+    with open(FILENAME, "w") as new_infile:
+        for d in guitars:
+            print(f"{d.name}, {d.year}, {d.cost}", file=new_infile)
 
 
 
